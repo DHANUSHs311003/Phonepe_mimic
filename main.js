ed0eebe792f6e886a28648229d93ed12f95edce9
DHANUSHs311003 <dhanushphoenix2003@gmail.com>
2023-12-15 16:17:43 +0530

    completed the payment process

--- main.js
+++ main.js
@@ -18,39 +18,48 @@ let text2 = document.createElement("p");
 text2.style.textAlign = "center";
 text2.style.fontWeight = "bolder";
 
-payBtn.addEventListener('click', function() {
-        let userName = document.getElementById("userName");
-        let userNumber = document.getElementById("userNumber");
-        let userMoney = document.getElementById("userMoney");
-    if (userName.value === "" || userNumber.value === "" || userMoney.value === "" ) {
-        alert("please enter all the details");
-    }
 
-    else {
+payBtn.addEventListener('click', function() {
+    let userName = document.getElementById("userName");
+    let userNumber = document.getElementById("userNumber");
+    let userMoney = document.getElementById("userMoney");
+
+    if (userName.value === "" || userNumber.value === "" || userMoney.value === "") {
+        alert("Please enter all the details");
+    } else {
         loginDiv.style.display = 'none';
         transferDiv.style.display = 'block';
-        text1.innerText = `Amount of  ₹${userMoney.value} transferred successfully to ${userName.value}`;
-        text2.innerText="Amount transferred failed due to insufficient balance";
-        let moneyPromise = new Promise((resolve, reject) => {
-            if(userMoney.value <= 10000) {
-                    setTimeout(()=> {
-                        resolve("success");
-                        processImg.src ="https://media.tenor.com/bm8Q6yAlsPsAAAAj/verified.gif";
-                        container_2.append(text1);
-                    },2000)
-            }
+        text1.innerText = `Amount of ₹${userMoney.value} transferred successfully to ${userName.value}`;
+        text2.innerText = "Amount transferred failed due to insufficient balance";
 
-            else {
-                setTimeout(()=> {
+        let moneyPromise = new Promise((resolve, reject) => {
+            if (userMoney.value <= balance && balance > 0) {
+                setTimeout(() => {
+                    resolve("success");
+                }, 2000);
+            } else  {
+                setTimeout(() => {
                     reject("failed");
-                    processImg.src ="https://i0.wp.com/nrifuture.com/wp-content/uploads/2022/05/comp_3.gif?fit=800%2C600&ssl=1";
-                    container_2.append(text2);
-                },2000)
+                }, 2000);
             }
         });
-    }
 
+        moneyPromise.then(() => {
+            // Update balance on success
+            balance -= parseInt(userMoney.value);
+            userBalance.textContent = `Your current account balance is ₹${balance}`;
+            processImg.src = "https://media.tenor.com/bm8Q6yAlsPsAAAAj/verified.gif";
+            container_2.append(text1);
+        }).catch(() => {
+            // Update balance on failure
+            balance = parseInt(balance);
+            userBalance.textContent = `Your current account balance is ₹${balance}`;
+            processImg.src = "https://i0.wp.com/nrifuture.com/wp-content/uploads/2022/05/comp_3.gif?fit=800%2C600&ssl=1";
+            container_2.append(text2);
+        });
+    }
 });
+
 
 home.addEventListener('click', function() {
     loginDiv.style.display = 'block';
@@ -58,10 +67,7 @@ home.addEventListener('click', function() {
     let userName = document.getElementById("userName");
     let userNumber = document.getElementById("userNumber");
     let userMoney = document.getElementById("userMoney");
-    let currentBal = balance - userMoney.value;
-    balance = currentBal;
-    userBalance.textContent = `Your current account balance is ${currentBal}`
-     userName.value = "";
-     userMoney.value = "";
-     userNumber.value ="";
+    userName.value = "";
+    userMoney.value = "";
+    userNumber.value = "";
 });
